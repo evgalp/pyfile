665c9b7167add564e694151cafdf606b63a45ad1
evgalp <evgeniyalpayev@gmail.com>
2017-10-12 15:02:49 +0300

    console version

--- pyfile/logic.py
+++ pyfile/logic.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from sys import argv
 
 def file_exists(in_file):
     """Input: file name, string
@@ -7,20 +8,17 @@ def file_exists(in_file):
     """
     return os.path.exists(in_file) and os.path.isfile(in_file)
 
-def create_file(filename):
-    """Input: file name, string
-    Return: --
-    """
-    newfile = open(filename, 'w')
-
-def copy_file(in_path, out_path):
+def copy_file():
     """Input: input file name, string; output file name, string;
     Copies file content from in_path to out_path
     Return: --
     """
 
+    in_path = input("Input file: ")
+    out_path = input("Out file: ")
+
     if file_exists(in_path):
-        in_file = open(in_path)
+        in_file = open(in_path, 'r')
         in_data = in_file.read()
 
         out_file = open(out_path, 'w')
@@ -28,7 +26,10 @@ def copy_file(in_path, out_path):
 
         out_file.close()
         in_file.close()
-    else:
-        print ("File not exists")
 
-copy_file("exists.txt", "dest.txt")
+        print("Copy done")
+    else:
+        print ("Input ot output path not exists")
+
+if __name__ == '__main__':
+    copy_file()
