e51dea7d70fcd296d54bb6cfa0b4abbef367c914
evgalp <evgeniyalpayev@gmail.com>
2017-10-12 14:31:01 +0300

    copy content of a file from current folder to other file in current folder

--- tests/main_tests.py
+++ tests/main_tests.py
@@ -1,2 +1,19 @@
 from nose.tools import *
-from pyfile.main import * 
+from pyfile.main import *
+import os
+
+# print(str(os.getcwd())
+
+def test_first():
+    assert_equal("1", "1")
+
+def test_file_exists():
+    existing_file = "E:\\work_evg\\tutorial\\python\\pyfile\\tests\\exists.txt"
+    not_existing_file = "notexists.txt"
+    assert_equal(file_exists(existing_file), True)
+    assert_equal(file_exists(not_existing_file), False)
+
+
+    print(os.path.abspath(os.path.curdir))
+    assert_equal(file_exists(existing_file), True)
+    assert_equal(file_exists(not_existing_file), False)
